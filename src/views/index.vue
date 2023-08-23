7bb28152764899439cdc51f0ddffe741bf90e89d
story3239~ <3239312337@qq.com>
2023-08-23 16:32:27 +0800

    feat(oneee): 项目规则

---
+++ src/views/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="index"></div>
+</template>
+
+<script lang="ts" setup>
+//@ts-nocheck
+import { useRouter, useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+import { ref, reactive, computed, watch } from 'vue'
+
+// 引入路由
+const router = useRouter()
+// 路由对象
+const route = useRoute()
+// 引入仓库
+const store = useStore()
+
+// 声明响应式数据
+const data = reactive({})
+
+// 父传子
+const props = defineProps({})
+
+// 子传父, 传递的自定义事件名还要在defineEmits中声明
+const emit = defineEmits([])
+</script>
+
+<style lang="scss" scoped>
+.index {
+  width: 100%;
+}
+</style>
